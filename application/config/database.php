0160add60a60a9e8fabc435fa81c4fd513e9dfad
Richard Casrto <68676125+richcastro82@users.noreply.github.com>
2022-12-18 18:53:39 -0600

    azure

--- application/config/database.php
+++ application/config/database.php
@@ -9,7 +9,7 @@ $db['default'] = array(
     'username' => 'vljbqdeuug', // The username to login with
     'password' => 'P26E4764U3P5M2JF$', // The password to login with
     'database' => 'tag-fileshare-database', // The name of the database
-    'MYSQL_CLIENT_SSL' => 'DigiCertGlobalRootCA.crt.pem',
+    // 'MYSQL_CLIENT_SSL' => 'DigiCertGlobalRootCA.crt.pem',
 
     /*
     !!! Do not edit anything below without knowing what you're doing !!!
